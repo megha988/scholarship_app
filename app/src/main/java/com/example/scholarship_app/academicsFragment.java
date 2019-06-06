3a63d5058da62266d9bd2480b2e642d650ddf6ae
ousnik <42760850+ousnik@users.noreply.github.com>
2019-06-06 13:06:40 +0530

    commit 4

--- app/src/main/java/com/example/scholarship_app/academicsFragment.java
+++ app/src/main/java/com/example/scholarship_app/academicsFragment.java
@@ -6,12 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class academicsFragment extends Fragment {
+
+    Spinner sem;
+
+    int sno[] = {1,2,3,4,5,6,7,8};
+
+    ArrayAdapter<String> adapter;
+    int record = 0;
+    TextView display_data;
 
 
     public academicsFragment() {
@@ -23,7 +35,49 @@ public class academicsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_academics, container, false);
+        final View v = inflater.inflate(R.layout.fragment_academics, container, false);
+
+//        sem = v.findViewById(R.id.spinner);
+//
+//        adapter = new  ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1);
+//
+//        sem.setAdapter(adapter);
+//
+//        display_data = v.findViewById(R.id.mark);
+//
+//        sem.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//                switch (i) {
+//
+//                    case 0:
+//                        record = 1;
+//                        break;
+//
+//                    case 1:
+//                        record = 2;
+//                        break;
+//
+//                    case 2:
+//                        record = 3;
+//                        break;
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//
+//        });
+        return v;
     }
+
+    public void mark(View view) {
+        display_data.setText(record);
+    }
+
 
 }
